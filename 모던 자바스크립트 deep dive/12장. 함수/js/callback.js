ea5f25df7fb4d59d408dec76e374d02d8c6f9562
Eunicekk <kate4234@gmail.com>
2024-03-19 23:08:16 +0900

    [12 챕터] 김윤

---
+++ 모던 자바스크립트 deep dive/12장. 함수/js/callback.js
@@ -0,0 +1,29 @@
+function findName(name) {
+    console.log('what is your name?'); //--- (1)
+    console.log('...finding');  //--- (2)
+    setTimeout(() => {
+        console.log('your name is ' + name); //--- (3)
+    }, 3000);
+	console.log('right?'); //--- (4)
+}
+
+findName('홍길동');
+
+// ----
+
+// function findName2(name) {
+// 	if (!name) return console.log("Sorry, I don't know you");
+// 	console.log('your name is ' + name); //--- (3)
+// 	console.log('right?');  //--- (4)
+// }
+
+// function delay(id, cb) {
+//     console.log('what is your name?'); //--- (1)
+//     console.log('...finding'); //--- (2)
+//     setTimeout(() => {
+//         const data = { 1: '홍길동', 2: '스펀지밥' };
+//         cb(data[id]);
+//     }, 3000);
+// }
+
+// delay(1, findName2);
