a8a050e6bdec5fd7f728d42fa0b5eeba2d98a2ed
박상용 <psyok7@naver.com>
2024-07-16 19:16:30 +0900

    [프로그래머스] 소수 찾기

---
+++ JS 알고리즘/박상용/7월/소수찾기.cs
@@ -0,0 +1,64 @@
+﻿class Solution
+{
+    static bool[] visited;
+    static HashSet<int> combnum;
+    static int len, answer = 0;
+    static List<char> numList;
+    static bool[] pri = new bool[10000000];
+
+    public int solution(string numbers)
+    {
+        Array.Fill(pri, true);
+        pri[0] = false;
+        pri[1] = false;
+
+        for (int i = 2; i * i < pri.Length; i++)
+        {
+            if (pri[i])
+            {
+                for (int j = i * i; j < pri.Length; j += i)
+                {
+                    pri[j] = false;
+                }
+            }
+        }
+
+        combnum = new HashSet<int>();
+        numList = numbers.ToList();
+
+        for (int i = 1; i <= numbers.Length; i++)
+        {
+            len = i;
+            visited = new bool[numbers.Length];
+            comb(0, "");
+        }
+
+        return answer;
+    }
+
+    private void comb(int count, string s)
+    {
+        if (count == len)
+        {
+            if (s[0] != '0')
+            {
+                int num = int.Parse(s);
+                if (pri[num]) { 
+                     answer++;
+                    pri[num] = false;
+                }
+            }
+            return;
+        }
+
+        for (int i = 0; i < numList.Count; i++)
+        {
+            if (!visited[i])
+            {
+                visited[i] = true;
+                comb(count + 1, s + numList[i]);
+                visited[i] = false;
+            }
+        }
+    }
+}
